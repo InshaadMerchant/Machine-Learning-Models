66d079483f6091497c81f42d11995f8ee9afee8b
InshaadMerchant <114313560+InshaadMerchant@users.noreply.github.com>
2024-09-25 23:12:36 -0500

    updated linear_regression.py

--- assignment2/naive_bayes.py
+++ assignment2/naive_bayes.py
@@ -1,9 +1,8 @@
-import sys
 import numpy as np
 import pandas as pd
 from math import sqrt, exp, pi
 
-def read_data(training_file, test_file):
+def naive_bayes(training_file, test_file):
     # Read training data
     training_data = pd.read_csv(training_file, header=None, delim_whitespace=True)
     training_features = training_data.iloc[:, :-1]
@@ -24,7 +23,14 @@ def read_data(training_file, test_file):
             'prior': len(class_data) / len(training_labels)
         }
 
-    return parameters, training_features, training_labels, test_features, test_labels
+    predictions = classify(parameters, test_features, test_labels)
+    # Output results to the screen
+    for prediction in predictions:
+        index, predicted, probability, true_label = prediction
+        accuracy = 1.0 if predicted == true_label else 0.0
+        print(f"ID={index}, predicted={predicted}, probability = {probability:.4f}, true={true_label}, accuracy={accuracy:.2f}")
+    accuracy = np.mean([1 if pred[1] == true_label else 0 for pred in predictions])
+    print(f"classification accuracy={accuracy:.4f}")
 
 def gaussian_probability(x, mean, std):
     return (1 / (sqrt(2 * pi) * std)) * exp(-((x-mean)**2 / (2 * std**2)))
@@ -40,26 +46,3 @@ def classify(parameters, test_features, test_labels):
         probability = np.exp(max(class_probs.values()))
         results.append((index + 1, best_class, probability, test_labels.iloc[index]))
     return results
-
-def main():
-    if len(sys.argv) > 4:
-        print("Invalid Usage: there should be atmost 3 arguments specified")
-
-    training_file = sys.argv[1]
-    test_file = sys.argv[2]
-    results_file = sys.argv[3]
-
-    params, training_features, training_labels, test_features, test_labels = read_data(training_file, test_file)
-    predictions = classify(params, test_features, test_labels)
-    
-    # Writing results to a file in the main function
-    with open(results_file, 'w') as file:
-        for prediction in predictions:
-            index, predicted, probability, true_label = prediction
-            accuracy = 1.0 if predicted == true_label else 0.0
-            file.write(f"ID={index}, predicted={predicted}, probability = {probability:.4f}, true={true_label}, accuracy={accuracy:.2f}\n")
-        accuracy = np.mean([1 if pred[1] == True else 0 for pred in predictions])
-        file.write(f"classification accuracy={accuracy:.4f}\n")
-
-if __name__ == "__main__":
-    main()
